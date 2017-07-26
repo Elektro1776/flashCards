1c85bda9a2cf08fb4218453092736360820e30fd
Austin <elektricwebdesign@gmail.com>
2017-07-26 14:25:24 -0600

    dickin around with promise chaining and rescursion, makin  a new branch before i destroy everything

--- app/src/basicFlash.js
+++ app/src/basicFlash.js
@@ -3,19 +3,75 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: 'Would you like to create a basic flash card? > y/n ',
+    terminal: false,
+    // prompt: 'Would you like to create a basic flash card? > y/n ',
 });
 
+let cardsArray = [];
 const createBasicCard = (dB, spec) => {
     const that = {};
     const mysql = dB;
-
-    function determineCardQuestion() {
-
+    // let numberOfCards = 0;
+    // const cardQuestion = "What is the card Question?";
+    // let cardAnswer;
+    function cardRecurs(numberOfCards) {
+        console.log(' WHAT ARE THE NUMBER OF CARDS ', numberOfCards);
+        const currentCardCount = numberOfCards;
+        return new Promise((resolve, reject) => {
+            if (currentCardCount === 0) {
+              console.log(' WHAT IS THE CARDS ARRAY???', cardsArray, resolve);
+                debugger;
+                resolve(cardsArray);
+            } else {
+                determineCardQuestion().then((card) => {
+                  console.log(' DO WE HAVE A CARD?', card);
+                  cardsArray.push(card);
+                  cardRecurs(numberOfCards - 1)
+                }).catch((err) => console.warn("errrrrr ", err))
+            }
+        });
     }
+    function determineNumberOfCards() {
+      rl.pause();
+        return new Promise((resolve, reject) => {
+            rl.question('How many cards would you like to create?', (answer) => {
+                // numberOfCards = 0
+                resolve(Promise.all([cardRecurs(parseInt(answer, 10))]));
+                // resolve()
+                // rl.close();
+            });
+        }).catch(err => console.warn('Error determining card number', err));
+    }
+    function determineCardQuestion() {
+        return new Promise((resolve, reject) => {
+            rl.question('What is the question?', (question) => {
+              // console.log(' DOES THIS FIRE SECOND TIME AROUND ?', question);
+              rl.write(process.stdout.write('\n'));
+              rl.question('And the answer is?', (answer) => {
+                  const cardObj = {};
+                  cardObj.front = question;
+                  cardObj.back = answer;
+                  rl.pause();
+                  resolve(cardObj);
+              });
+            });
+        }).catch(err => console.warn('Error determining card number', err));
+    }
+    function createCard(question) {
+        return new Promise((resolve, reject) => {
+            rl.question('And the answer is?', (answer) => {
+                const cardObj = {};
+                cardObj.front = question;
+                cardObj.back = answer;
+                resolve(cardObj);
+                rl.close();
+            });
+        }).catch(err => console.warn('Error creating your card, Please try again.', err));
+    }
+    that.determineNumberOfCards = determineNumberOfCards;
     that.determineCardQuestion = determineCardQuestion;
-
+    that.createCard = createCard;
     return that;
-}
+};
 
 module.exports = createBasicCard;
--- index.js
+++ index.js
@@ -3,32 +3,40 @@ const readline = require('readline');
 const DBConfig = require('./config');
 const myCon = mysql.createConnection(DBConfig);
 const createBasicFlash = require('./app/src/basicFlash')(myCon);
-const createCloze = require('./app/src/cloze-deleted')(myCon);
-
+// const createCloze = require('./app/src/cloze-deleted')(myCon);
+//
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: 'Would you like to create a basic flash card? > y/n '
 });
 let basicFlashQuestion = "Would you like to create a basic flash card? y/n ";
-let clozeDeleted = "Would you like to create a Cloze Deleted Flash card? y/n ";
-
-rl.prompt();
-
-rl.on('line', (line) => {
+// process.stdin.resume();
+// let clozeDeleted = "Would you like to create a Cloze Deleted Flash card? y/n ";
+//
+// rl.prompt();
+//
+rl.question(basicFlashQuestion, (line) => {
     switch (line.trim()) {
     case 'y':
-        console.log('CREATE THE BASIC CARD', createBasicFlash);
+        console.log('CREATE THE BASIC CARD', line, createBasicFlash);
+        rl.pause();
+        return createBasicFlash.determineNumberOfCards()
+        .then(numberOfCards => console.log(' WHAT THE HELL WILL THIS BE ?', numberOfCards))
+        .catch((err) => console.warn("errrrrrrr", err));
+        // .then(question => createBasicFlash.createCard(question))
+        // .then((cardsToReview) => {
+        //   console.log(' WHAT IS THE CARD OBJECT RETURNED', cardsToReview);
+        // });
 
-        break;
+        // break;
     case 'n': {
         return rl.question(clozeDeleted, (answer) => {
             if (answer === 'y') {
                 console.log("WHAT IS THE CLozeeee", createCloze);
             }
 
-        rl.clearLine(answer, 1)
-        rl.close()
+        // rl.clearLine(answer, 1)
       })
     }
     break;
@@ -37,22 +45,15 @@ rl.on('line', (line) => {
       break;
   }
   // rl.prompt();
-}).on('close', () => {
-  rl.write("Fine have a dandy day! \n");
-  process.exit(0);
-});
+})
 // rl.question('Would you like to create a Flash Card? y/n', (answer) => {
 //   // TODO: Log the answer in a database
-//   switch (answer) {
-//     case 'y':
-//       rl.question(basicFlashQuestion + " y/n", (answer) => {
-//         if('y') {
-//           return console.log(' WE HAVE A YES!');
-//         } else {
-//           return console.log(' WE HAVE A NO');
-//         }
-//
-//       })
+//   console.log(' WHAT IS THE ANWER', answer);
+//   switch (answer.trim()) {
+//     case 'y':{
+//       console.log(' Should get  a y???', answer);
+//       createBasicFlash.determineCardQuestion();
+//     }
 //       break;
 //     case 'no' :
 //     rl.write("Fine don't help yourself!\n");
@@ -61,12 +62,23 @@ rl.on('line', (line) => {
 //
 //   }
 //
-//   rl.close();
+//   //rl.close();
 // });
-// con.connect((err) => {
-//   if (err) console.warn("Error",err);
-//   con.query("DROP TABLE IF EXISTS customers", (err, result, fields) => {
-//     if (err) throw err;
-// console.log("Number of records deleted: " ,result)
-//   });
-// });
+// // con.connect((err) => {
+// //   if (err) console.warn("Error",err);
+// //   con.query("DROP TABLE IF EXISTS customers", (err, result, fields) => {
+// //     if (err) throw err;
+// // console.log("Number of records deleted: " ,result)
+// //   });
+// // });
+        // rl.prompt();
+        // rl.question("What is the question?", (answer) => {
+        //   console.log(' ANSWER ???', answer);
+        //   rl.setPrompt('>>');
+        //   rl.prompt()
+        //   // Simulate ctrl+u to delete the line written previously
+        // // rl.write(null, {ctrl: true, name: 'u'})
+        // })
+        // rl.on('line', (input) => {
+        //   console.log(' WHAT IS THE INPUT?', input);
+        // })
