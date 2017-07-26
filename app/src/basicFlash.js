fc7bc323bc861fa0ec121b77b53c405c33118e75
Austin <elektricwebdesign@gmail.com>
2017-07-26 16:00:47 -0600

    PROMISE CHAIN SET UP CAN NOW ASK MULTIPLE QUESTIONS

--- app/src/basicFlash.js
+++ app/src/basicFlash.js
@@ -11,65 +11,100 @@ let cardsArray = [];
 const createBasicCard = (dB, spec) => {
     const that = {};
     const mysql = dB;
-    // let numberOfCards = 0;
-    // const cardQuestion = "What is the card Question?";
-    // let cardAnswer;
-    function cardRecurs(numberOfCards) {
-        console.log(' WHAT ARE THE NUMBER OF CARDS ', numberOfCards);
-        const currentCardCount = numberOfCards;
+    function prompt(question, callback) {
         return new Promise((resolve, reject) => {
-            if (currentCardCount === 0) {
-              console.log(' WHAT IS THE CARDS ARRAY???', cardsArray, resolve);
-                debugger;
-                resolve(cardsArray);
-            } else {
-                determineCardQuestion().then((card) => {
-                  console.log(' DO WE HAVE A CARD?', card);
-                  cardsArray.push(card);
-                  cardRecurs(numberOfCards - 1)
-                }).catch((err) => console.warn("errrrrr ", err))
-            }
+            const stdin = process.stdin;
+            const stdout = process.stdout;
+
+            stdin.resume();
+            stdout.write(question);
+
+            stdin.once('data', (data) => {
+              // callback(data.toString().trim());
+                resolve(data.toString().trim());
+            });
         });
     }
-    function determineNumberOfCards() {
-      rl.pause();
+    function startBasicCardCreation() {
+        rl.pause();
         return new Promise((resolve, reject) => {
             rl.question('How many cards would you like to create?', (answer) => {
-                // numberOfCards = 0
-                resolve(Promise.all([cardRecurs(parseInt(answer, 10))]));
-                // resolve()
-                // rl.close();
+                Promise.resolve()
+                .then(
+                    () => {
+                        // console.group( "Recursion - inline function." );
+                        return (parseInt(answer, 10));
+                    },
+           )
+           // In this link of the promise chain, the resolution function is explicitly
+           // named. This way, the function reference can be invoked from within its
+           // own function body.
+           .then(
+               function recursiveFunction(n) {
+                 rl.pause();
+                  //  console.log( "Entering recursive function for [", n, "]." );
+                   // Once we hit zero, bail out of the recursion. The key to recursion
+                   // is that it stops at some point, and the callstack can be "rolled"
+                   // back up.
+                   if (n === 0) {
+                       return (0);
+                   }
+                   // Internally to this promise link, we're going to start a NEW
+                   // PROMISE CHAIN whose fulfillment will become the continuation of
+                   // the parent promise chain. This fundamentally different from the
+                   // reduce() example.
+                   const promise = createCard().then(
+                        (card) => {
+                            cardsArray.push(card);
+                            return (recursiveFunction(n - 1)); // RECURSE!
+                        },
+                   );
+                   return (promise);
+                   // NOTE: To be ultra-concise, I could have written the following;
+                   // but, I think that would have made the example harder to follow.
+                   // --
+                   // return( Promise.resolve( recursiveFunction( n - 1 ) ) );
+               },
+           )
+           .then(
+               (values) => {
+                  //  console.groupEnd();
+                   rl.resume();
+                   rl.close();
+                   resolve(cardsArray);
+               },
+           )
+       ;
             });
         }).catch(err => console.warn('Error determining card number', err));
     }
-    function determineCardQuestion() {
+    function createCard() {
         return new Promise((resolve, reject) => {
-            rl.question('What is the question?', (question) => {
-              // console.log(' DOES THIS FIRE SECOND TIME AROUND ?', question);
-              rl.write(process.stdout.write('\n'));
-              rl.question('And the answer is?', (answer) => {
-                  const cardObj = {};
-                  cardObj.front = question;
-                  cardObj.back = answer;
-                  rl.pause();
-                  resolve(cardObj);
-              });
-            });
+
+            prompt("What is the question for you card?").then((question) => {
+                prompt('What is the answer?').then((answer) => {
+                    resolve({ front: question, back: answer })
+                })
+            })
+            // rl.resume();
+            // rl.write(process.stdout.write('\n'));
+            // rl.question('What is the question?', (question) => {
+            //     // rl.pause();
+            //     rl.write(process.stdout.write('\n'));
+            //
+            //     rl.question('And the answer is?', (answer) => {
+            //         const cardObj = {};
+            //         cardObj.front = question;
+            //         cardObj.back = answer;
+            //         // rl.write(process.stdout.write('\n'));
+            //
+            //         rl.pause();
+            //         resolve(cardObj);
+            //     });
+            // });
         }).catch(err => console.warn('Error determining card number', err));
     }
-    function createCard(question) {
-        return new Promise((resolve, reject) => {
-            rl.question('And the answer is?', (answer) => {
-                const cardObj = {};
-                cardObj.front = question;
-                cardObj.back = answer;
-                resolve(cardObj);
-                rl.close();
-            });
-        }).catch(err => console.warn('Error creating your card, Please try again.', err));
-    }
-    that.determineNumberOfCards = determineNumberOfCards;
-    that.determineCardQuestion = determineCardQuestion;
+    that.startBasicCardCreation = startBasicCardCreation;
     that.createCard = createCard;
     return that;
 };
--- index.js
+++ index.js
@@ -6,79 +6,35 @@ const createBasicFlash = require('./app/src/basicFlash')(myCon);
 // const createCloze = require('./app/src/cloze-deleted')(myCon);
 //
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: 'Would you like to create a basic flash card? > y/n '
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Would you like to create a basic flash card? > y/n ',
+    terminal: false,
 });
-let basicFlashQuestion = "Would you like to create a basic flash card? y/n ";
-// process.stdin.resume();
-// let clozeDeleted = "Would you like to create a Cloze Deleted Flash card? y/n ";
-//
-// rl.prompt();
-//
+const basicFlashQuestion = 'Would you like to create a basic flash card? y/n ';
+const clozeDeleted = 'Would you like to create a Cloze Deleted Flash card? y/n ';
+
 rl.question(basicFlashQuestion, (line) => {
     switch (line.trim()) {
     case 'y':
         console.log('CREATE THE BASIC CARD', line, createBasicFlash);
-        rl.pause();
-        return createBasicFlash.determineNumberOfCards()
-        .then(numberOfCards => console.log(' WHAT THE HELL WILL THIS BE ?', numberOfCards))
-        .catch((err) => console.warn("errrrrrrr", err));
-        // .then(question => createBasicFlash.createCard(question))
-        // .then((cardsToReview) => {
-        //   console.log(' WHAT IS THE CARD OBJECT RETURNED', cardsToReview);
-        // });
+        rl.close();
+        return createBasicFlash.startBasicCardCreation()
+        // thenable now responds with cardsArray,
+        // TODO: set up the prompt to review the cards created.
+        .then(cardsArray => console.log('CARDS ARRAY', cardsArray))
+        .catch(err => console.warn('errrrrrrr', err));
 
-        // break;
     case 'n': {
         return rl.question(clozeDeleted, (answer) => {
             if (answer === 'y') {
-                console.log("WHAT IS THE CLozeeee", createCloze);
             }
 
         // rl.clearLine(answer, 1)
-      })
+        });
     }
-    break;
     default:
-      rl.close();
-      break;
-  }
-  // rl.prompt();
-})
-// rl.question('Would you like to create a Flash Card? y/n', (answer) => {
-//   // TODO: Log the answer in a database
-//   console.log(' WHAT IS THE ANWER', answer);
-//   switch (answer.trim()) {
-//     case 'y':{
-//       console.log(' Should get  a y???', answer);
-//       createBasicFlash.determineCardQuestion();
-//     }
-//       break;
-//     case 'no' :
-//     rl.write("Fine don't help yourself!\n");
-//     break;
-//     default:
-//
-//   }
-//
-//   //rl.close();
-// });
-// // con.connect((err) => {
-// //   if (err) console.warn("Error",err);
-// //   con.query("DROP TABLE IF EXISTS customers", (err, result, fields) => {
-// //     if (err) throw err;
-// // console.log("Number of records deleted: " ,result)
-// //   });
-// // });
-        // rl.prompt();
-        // rl.question("What is the question?", (answer) => {
-        //   console.log(' ANSWER ???', answer);
-        //   rl.setPrompt('>>');
-        //   rl.prompt()
-        //   // Simulate ctrl+u to delete the line written previously
-        // // rl.write(null, {ctrl: true, name: 'u'})
-        // })
-        // rl.on('line', (input) => {
-        //   console.log(' WHAT IS THE INPUT?', input);
-        // })
+        rl.close();
+        break;
+    }
+});
